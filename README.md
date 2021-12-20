b2901812d278bd7ae3645e693426afec2077eedf
Apurbo20 <devilsaif20@gmail.comm>
2021-12-20 21:14:41 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Medent : You Choose Your Dental Clinic, We Support.
+
+## Live Link 
+
+Visit our site  [Medent](https://medent-dental-clinic.web.app/)
+
+
+### About Project 
+- I've used react to develop the UI of this site
+- I used react-font-awesome for icons. And also used react bootstrap for responsiveness and faster design.
+- I created a fake-data-json file, placed it locally and github loaded data from it to show in the UI
+- Context api was used to get data easily from all the route
+- Used Private Route, Google SignIn and Email Sign in , Sign Up for Authentication
+- After Sign in user name and image show the Navber
---
+++ src/Components/OurDoctors/Doctors.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import UseDoctors from '../../Hooks/UseDoctors';
+import Doctor from '../Doctor/Doctor';
+import Newslater from '../Home/Newslater/Newslater';
+import bgdoctor from '../../image/banner/allwebsite.png';
+
+const Doctors = () => {
+    const [doctors] = UseDoctors();
+    return (
+        <>
+            <section className="mb-5" style={{ background: `url(${bgdoctor})` }}>
+                <div className="container shadow-lg px-2 py-3">
+                    <h2 className="text-center fw-bolder text-white">Our Popular Doctor</h2>
+                    <hr className="high mx-auto"></hr>
+                    <Row xs={1} md={3} className="g-4 mt-3">
+                        {
+                            doctors.map(doctor => (<Doctor
+                                key={doctor.id}
+                                doctor={doctor}
+                            ></Doctor>))
+                        }
+                    </Row>
+                </div>
+            </section>
+            <Newslater></Newslater>
+        </>
+    );
+};
+
+export default Doctors;
